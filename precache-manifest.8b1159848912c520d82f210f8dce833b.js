14da6489d19b72eaa284baff9c4e86e07caa556c
soumilk <soumilk.k@gmail.com>
2020-06-28 00:19:20 +0530

    Updates

---
+++ precache-manifest.8b1159848912c520d82f210f8dce833b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e599367d8b81da33b4b6028e7eb956cc",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "b19c9adf97c0cdcf3813",
+    "url": "/Tic-Tac-Toe/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b19c9adf97c0cdcf3813",
+    "url": "/Tic-Tac-Toe/static/js/2.49f91ed2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Tic-Tac-Toe/static/js/2.49f91ed2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "779de0c1147c7a28f9a6",
+    "url": "/Tic-Tac-Toe/static/js/main.3ed1dcf7.chunk.js"
+  },
+  {
+    "revision": "5965f36d7f72120b2bad",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.49965c0b.js"
+  }
+]);
